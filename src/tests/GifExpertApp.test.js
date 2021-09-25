052dccd0783bcd06ba325bc9c62540ea84107a64
eitan <eitanmb@gmail.com>
2021-09-25 10:44:27 -0300

    new run build

--- src/tests/GifExpertApp.test.js
+++ src/tests/GifExpertApp.test.js
@@ -12,6 +12,18 @@ describe('Pruebas en GifExpertApp', () => {
         expect( wrapper ).toMatchSnapshot();
         
     });
+
+    test('debe mostrar una lista de categories', () => {
+        
+        const categories = ['Zelda', 'Mario'];
+
+        const wrapper = shallow(<GifExpertApp defaultCategories={ categories }/>);
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('GifGrid').length ).toBe( categories.length +1 );
+
+    });
+    
     
     
 });
